d2290ad27eb441a42ca9fbb9fb1ff64eb6963f98
Florian Gößler <florian.goessler@immobilienscout24.de>
2015-07-07 14:39:38 +0200

    improved aggregate-script

--- aggregate-script.sh
+++ aggregate-script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 declare -a arr=("Comedy" "Drama" "Action" "Documentary" "Adult" "Romance" "Thriller" "Animation" "Family" "Horror" "Music" "Crime" "Adventure" "Fantasy" "Sci-Fi" "Mystery" "Biography" "History" "Sport" "Musical" "War" "Western" "News" "Reality-TV")
 
-for i in "${arr[@]}"
-#for i in `seq 0 5 100`;
+for i in `seq 0 5 100`;
 do
-	#cat res$i/* > res$i.txt
-	grep $i res*.txt | sed "s/,/;/g" > rescollected/tmp.csv
-	./transpose.py < rescollected/tmp.csv | sed -e 's/^/'"$i"';/' | sed '1d' > rescollected/$i.csv
+	cat res$i/* > res$i.txt
+done
+
+for i in "${arr[@]}"
+do
+	grep $i, res*.txt | sed "s/,/;/g" > rescollected/$i.tmp.csv
+	./transpose.py < rescollected/$i.tmp.csv | sed -e 's/^/'"$i"';/' | sed '1d' > rescollected/$i.csv
+	rm rescollected/$i.tmp.csv
 done
 
 cat rescollected/* > rescollected.csv
